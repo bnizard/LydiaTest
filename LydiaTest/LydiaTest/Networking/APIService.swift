ee8f938326b5b46adb00092a10a392423236eb93
bastien <bastien.nizard@shopopop.com>
2025-03-29 11:50:26 +0100

    feat[LydiaTest]: add APIResponse model

---
+++ LydiaTest/LydiaTest/Networking/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  LydiaTest
+//
+//  Created by Bastien Nizard on 29/03/2025.
+//
+
+import Foundation
---
+++ LydiaTest/LydiaTest/ViewModel/ContactViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContactViewModel.swift
+//  LydiaTest
+//
+//  Created by Bastien Nizard on 29/03/2025.
+//
+
+import Foundation
