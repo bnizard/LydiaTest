c244ebcdef985f61b5fcb88160c31cde719eac58
bastien <bastien.nizard@shopopop.com>
2025-03-29 11:50:56 +0100

    feat[LydiaTest]: add APIService class

--- LydiaTest/LydiaTest/Networking/APIService.swift
+++ LydiaTest/LydiaTest/Networking/APIService.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class APIService {
+    static let shared = APIService()
+    private let url = "https://randomsuer.me/api/?results=10"
+
+    func fetchUsers(completion: @escaping (Result<[Contact], Error>) -> Void) {
+        guard let requestUrl = URL(string: url) else { return }
+
+        URLSession.shared.dataTask(with: requestUrl) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else { return }
+
+            do {
+                let decodeResponse = try JSONDecoder().decode(APIResponse.self, from: data)
+                completion(.success(decodeResponse.results))
+            } catch {
+                completion(.failure(error))
+            }
+
+        }.resume()
+
+    }
+}
