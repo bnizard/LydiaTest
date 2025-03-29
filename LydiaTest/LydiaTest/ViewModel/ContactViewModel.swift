7320939520b900df788a773b9b763b0530b11b7d
bastien <bastien.nizard@shopopop.com>
2025-03-29 11:51:16 +0100

    feat[LydiaTest]: add  ContactViewModel

--- LydiaTest/LydiaTest/ViewModel/ContactViewModel.swift
+++ LydiaTest/LydiaTest/ViewModel/ContactViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class ContactViewModel {
+    var contacts : [Contact] = []
+
+    func fetchContacts() {
+        APIService.shared.fetchUsers { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let contacts):
+                    self?.contacts.append(contentsOf: contacts)
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+            }
+        }
+
+    }
+}
